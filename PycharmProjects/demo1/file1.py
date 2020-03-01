f8401f8067b2fbcb4c4da888535d1da855699992
Pankaj Kumar <pankajkumarsingh28@gmail.com>
2020-03-01 11:38:26 +0530

    First_commit

---
+++ PycharmProjects/demo1/file1.py
@@ -0,0 +1,5 @@
+from selenium.webdriver import Chrome
+
+driver=Chrome("C:\\Users\\PANKAJ SINGH\\Desktop\\Python\\chromedriver_win32\\chromedriver.exe")
+driver.get('https://www.google.com/')
+driver.maximize_window()
